fe58c22b3024f644f6436a6e3e6f471bea42b6f0
MIbrahim4747 <147363306+MIbrahim4747@users.noreply.github.com>
2023-11-14 20:43:45 +0500

    first commit

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link"
+import AcmeLogo from "../ui/acme-logo"
+import { lusitana } from "../ui/fonts"
+
+export default function Page() {
+    return (
+        <h1>i am dashboard</h1>
+    )
+}
